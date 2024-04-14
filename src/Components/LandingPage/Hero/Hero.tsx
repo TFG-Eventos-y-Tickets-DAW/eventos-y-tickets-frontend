5f9863bc8d8f1044878a5aab7b8de4bcf834c6bb
KevinALopez <005.kevin.ayala@gmail.com>
2024-04-14 11:58:31 +0200

    minor changes to hero img

--- src/Components/LandingPage/Hero/Hero.tsx
+++ src/Components/LandingPage/Hero/Hero.tsx
@@ -4,8 +4,12 @@ import heroImg from "../../../assets/Hero.jpg";
 export default function Hero() {
     return (
         <div className="relative">
-            <img src={heroImg} alt="Here should be a hero image." />
-            <div className="absolute bottom-[0.5px] h-52 flex flex-col items-center justify-center gap-4">
+            <img
+                src={heroImg}
+                alt="Here should be a hero image."
+                className="w-full"
+            />
+            <div className="pb-3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full h-52 flex flex-col items-center justify-center gap-3">
                 <h1 className="font-karla font-bold text-white text-5xl text-center text-stroke-3">
                     What are you looking for?
                 </h1>
